abf0c78a711029528e3908f3c0b0d9b0fefbed2e
Oscar Garrido <oscargarrido.ortega@gmail.com>
2024-09-08 18:06:55 -0300

    mejoro pruebas al 100%

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -9,10 +9,6 @@ import { esPalindromo } from "../src/palindromo.js";
 
 describe("Test Suite App", () => {
 
-   /* test("endpoint /", () => {
-        expect(1 + 1).toBe(2);
-    });
-*/
     test("endpoint  /key", async () => {
         return await request(app)
         .get("/key")
@@ -25,10 +21,10 @@ describe("Test Suite App", () => {
     test("funcion palabra palindromo", () => {
         expect(esPalindromo("girafarig")).toBe(true);
         expect(esPalindromo("Farigiraf ")).toBe(true);
-        expect(esPalindromo("Dudunsparce ")).toBe(false);
+        expect(esPalindromo("Dudunsparce")).toBe(false);
 
     });
-    test("endpoint  /palindromo", async () => {
+    test("endpoint  /palindromo si", async () => {
         return await request(app)
         .get("/palindromo/girafarig")
         .expect("Content-Type", /text/)
@@ -37,12 +33,21 @@ describe("Test Suite App", () => {
                 expect(response.text).toBe("Hola, La frase ingresada es palindromo");
             })
     });
+    test("endpoint  /palindromo no", async () => {
+        return await request(app)
+        .get("/palindromo/Dudunsparce")
+        .expect("Content-Type", /text/)
+            .expect(200)
+            .then((response) => {
+                expect(response.text).toBe("Hola, La frase ingresada no es palindromo");
+            })
+    });
     test("Funcion Número Primo", () => {
         expect(esPrimo(2)).toBe(true);
         expect(esPrimo(1)).toBe(false);
         expect(esPrimo(8)).toBe(false);
     });
-    test("endpoint  /primo", async () => {
+    test("endpoint  /primo si", async () => {
         return await request(app)
         .get("/primo/7")
         .expect("Content-Type", /text/)
@@ -51,6 +56,15 @@ describe("Test Suite App", () => {
                 expect(response.text).toBe("Hola, el numero ingresado es un numero primo");
             })
     });
+    test("endpoint  /primo no", async () => {
+        return await request(app)
+        .get("/primo/8")
+        .expect("Content-Type", /text/)
+            .expect(200)
+            .then((response) => {
+                expect(response.text).toBe("Hola, el numero ingresado no es un numero primo");
+            })
+    });    
 
     test("test de endpoint /", async () => {
         return await request(app)
